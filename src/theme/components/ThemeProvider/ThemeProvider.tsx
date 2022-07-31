0547460b2993f96173b5415c53daa8ff1b4d06c3
Daniel Jarosz <74425129+danjarosz@users.noreply.github.com>
2022-07-31 12:39:10 +0200

    feat: introduce sass, implement theming and global styles (#2)

---
+++ src/theme/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,73 @@
+import type { FC, ReactNode } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
+import { Theme } from "../../types/theme";
+
+type SwitchTheme = () => void;
+
+interface ThemeContextValue {
+  theme: Theme;
+  switchTheme: SwitchTheme;
+}
+
+const defaultThemeContextValue = {
+  theme: Theme.light,
+  switchTheme: () => {},
+};
+
+export const ThemeContext = createContext<ThemeContextValue>(
+  defaultThemeContextValue
+);
+
+interface ThemeProviderProps {
+  children?: ReactNode;
+}
+
+const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+  const { children } = props;
+
+  const [theme, setTheme] = useState(Theme.light);
+
+  const changeTheme = useCallback((theme: Theme) => {
+    setTheme(theme);
+    localStorage.setItem("theme", theme);
+
+    const root = document.querySelector(":root") as HTMLElement;
+    root.setAttribute("color-scheme", `${theme}`);
+  }, []);
+
+  const getCurrentTheme = useCallback(() => {
+    let theme = window.matchMedia(`(prefers-color-scheme: ${Theme.dark})`)
+      .matches
+      ? Theme.dark
+      : Theme.light;
+
+    const themeFormLocalStorage = localStorage.getItem("theme");
+
+    if (
+      themeFormLocalStorage === Theme.light ||
+      themeFormLocalStorage === Theme.dark
+    ) {
+      changeTheme(themeFormLocalStorage);
+    } else {
+      changeTheme(theme);
+    }
+  }, [changeTheme]);
+
+  const switchTheme = useCallback(() => {
+    changeTheme(theme === Theme.light ? Theme.dark : Theme.light);
+  }, [changeTheme, theme]);
+
+  useEffect(() => {
+    getCurrentTheme();
+  }, [getCurrentTheme]);
+
+  return (
+    <div>
+      <ThemeContext.Provider value={{ theme, switchTheme }}>
+        {children}
+      </ThemeContext.Provider>
+    </div>
+  );
+};
+
+export default ThemeProvider;
