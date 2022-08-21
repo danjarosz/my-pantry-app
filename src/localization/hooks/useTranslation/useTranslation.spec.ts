3d6c6633e4e61b6130d8be31825c89eee23df82f
Daniel Jarosz <jarosz.daniel@protonmail.com>
2022-08-21 18:21:04 +0200

    tests(localization): cover

--- src/localization/hooks/useTranslation/useTranslation.spec.ts
+++ src/localization/hooks/useTranslation/useTranslation.spec.ts
@@ -1,5 +1,19 @@
 import { useTranslation } from "./useTranslation";
+import { renderHook } from "@testing-library/react";
+import { Lang } from "../../types";
 
 describe("useTranslation", () => {
-  it("", () => {});
+  it("should return correct values", () => {
+    const initialValues = {
+      lang: Lang.en,
+    };
+
+    const {
+      result: {
+        current: { lang },
+      },
+    } = renderHook(() => useTranslation());
+
+    expect(lang).toEqual(initialValues.lang);
+  });
 });
--- src/localization/utility/getInitialLang/getInitialLang.spec.ts
+++ src/localization/utility/getInitialLang/getInitialLang.spec.ts
@@ -1,5 +1,29 @@
 import { getInitialLang } from "./getInitialLang";
+import { Lang } from "../../types";
+import { langStorageKey } from "../../constants";
 
 describe("getInitialLang", () => {
-  it("", () => {});
+  afterAll(() => {
+    localStorage.removeItem(langStorageKey);
+  });
+
+  it("should return provided language", () => {
+    const expectedResult = Lang.pl;
+    const result = getInitialLang(Lang.pl);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it("should return language restored form local storage", () => {
+    localStorage.setItem(langStorageKey, Lang.pl);
+    const expectedResult = Lang.pl;
+    const result = getInitialLang();
+    expect(result).toEqual(expectedResult);
+  });
+
+  it("should return default language", () => {
+    localStorage.removeItem(langStorageKey);
+    const expectedResult = Lang.en;
+    const result = getInitialLang();
+    expect(result).toEqual(expectedResult);
+  });
 });
