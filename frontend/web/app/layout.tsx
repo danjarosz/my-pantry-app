6979f27b37a7ad402897e84814ce336f1c2370a5
Daniel Jarosz <jarosz.daniel@protonmail.com>
2024-03-02 15:58:09 +0100

    feat(Prettier): introduce to the front project

--- frontend/web/app/layout.tsx
+++ frontend/web/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "My Pantry App",
-  description: "A clever pantry management app",
+  title: 'My Pantry App',
+  description: 'A clever pantry management app'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
