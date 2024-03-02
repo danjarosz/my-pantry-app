d7d0e7d60d8e3b991ea85ece87c58915de8fcd23
Daniel Jarosz <jarosz.daniel@protonmail.com>
2024-03-02 19:56:28 +0100

    feat(StyleLint): introduce to the front project

--- frontend/web/app/page.tsx
+++ frontend/web/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          By <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
+          By <Image src="/vercel.svg" alt="Vercel Logo" className={styles['vercel-logo']} width={100} height={24} priority />
         </a>
       </div>
 
