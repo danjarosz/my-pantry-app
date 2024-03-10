2d3d3755c1d016f45d14405dd14fcc68fb4ca404
Daniel Jarosz <jarosz.daniel@protonmail.com>
2024-03-10 10:04:00 +0100

    clean up pages

--- frontend/web/app/page.tsx
+++ frontend/web/app/page.tsx
@@ -1,25 +1,7 @@
-import Image from 'next/image';
-import styles from './page.module.css';
-
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Welcome to My Pantry App</h1>
-      <p>A clever pantry management app</p>
-      <p>Powered</p>
-      <div>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By <Image src="/vercel.svg" alt="Vercel Logo" className={styles['vercel-logo']} width={100} height={24} priority />
-        </a>
-      </div>
-
-      <div className={styles.center}>
-        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-      </div>
     </main>
   );
 }
