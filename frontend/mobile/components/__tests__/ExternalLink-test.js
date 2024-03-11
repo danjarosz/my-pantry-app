e03d2a2aae001bc98436fe4f1f0c62984992c4fd
Daniel Jarosz <jarosz.daniel@protonmail.com>
2024-03-11 20:50:48 +0100

    feat(mobile): setup tests

---
+++ frontend/mobile/components/__tests__/ExternalLink-test.js
@@ -0,0 +1,17 @@
+import { ExternalLink } from '../ExternalLink';
+import { render, screen } from '@testing-library/react-native';
+
+describe('ExternalLink', () => {
+  it('should be defined', () => {
+    expect(ExternalLink).toBeDefined();
+  });
+
+  it('shoudl render anchor with correct label', () => {
+    render(<ExternalLink href="https://example.com">Example</ExternalLink>);
+    expect(
+      screen.getByRole('link', {
+        name: 'Example'
+      })
+    ).toBeTruthy();
+  });
+});
