7a7dd234fbe27d1b4dc20a6c97afa9ab70611ba5
Daniel Jarosz <74425129+danjarosz@users.noreply.github.com>
2024-03-11 20:56:00 +0100

    feat(mobile): setup tests (#15)

--- frontend/mobile/components/__tests__/StyledText-test.js
+++ frontend/mobile/components/__tests__/StyledText-test.js
@@ -2,8 +2,10 @@ import * as React from 'react';
 import renderer from 'react-test-renderer';
 
 import { MonoText } from '../StyledText';
+import { render } from '@testing-library/react-native';
 
 it(`renders correctly`, () => {
+  render(<MonoText>Snapshot test!</MonoText>);
   const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
 
   expect(tree).toMatchSnapshot();
