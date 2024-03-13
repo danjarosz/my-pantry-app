cf4c10795efc49f894919d85f7df95c81ca91673
Daniel Jarosz <74425129+danjarosz@users.noreply.github.com>
2024-03-13 23:01:09 +0100

    chore(Storybook): install (#17)

--- frontend/web/app/login/page.tsx
+++ frontend/web/app/login/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/lib/ui/Button/Button';
+
 export default function Login() {
   return (
     <main>
       <h1>Login page</h1>
+      <Button label={'Log in'} primary size={'large'} />
     </main>
   );
 }
