8a83201a2d894a91d33d54f2a37431c302d870b7
Daniel Jarosz <jarosz.daniel@protonmail.com>
2024-03-13 22:35:34 +0100

    fix problems

--- frontend/web/app/dashboard/page.tsx
+++ frontend/web/app/dashboard/page.tsx
@@ -1,9 +1,6 @@
-import { Header } from '@/lib/ui/Header/Header';
-
 export default function Dashboard() {
   return (
     <main>
-      <Header user={{ name: 'Jane Doe' }} />
       <h1>Dashboard</h1>
     </main>
   );
